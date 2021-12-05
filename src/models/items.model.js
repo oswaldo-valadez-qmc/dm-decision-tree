475a8a2cba499a26fb2fc823a0bd2fc3bf1ba3e5
Oswaldo-Valadez <Oswaldo-Valadez@users.noreply.github.com>
2021-12-04 20:05:34 -0600

    update

--- src/models/items.model.js
+++ src/models/items.model.js
@@ -2,39 +2,11 @@
 
 const { pool } = require("../utils/database/query");
 
-const inputs = [
-  // "rating_top",
-  // "reviews_text_count",
-  "genre",
-  "tag",
-  // "metacritic",
-  // "added",
-  "beaten",
-  "dropped",
-  // "owned",
-  "playing",
-  // "playtime",
-  // "rating",
-  // "ratings_count",
-  // "reviews_count",
-  // "suggestions_count",
-  // "toplay",
-  // "yet",
-];
+const inputs = ["genre", "tag", "beaten", "dropped", "playing"];
 
 const disc = { beaten: true, dropped: true, playing: true };
 
-const cond = `"(genre = 'Adventure'
-OR genre = 'Indie'
-OR genre = 'Family'
-OR genre = 'Racing'
-OR genre = 'Strategy'
-OR genre = 'Rpg')
-AND dropped > 50
-AND beaten > 29
-AND tag = 'Dark'
-AND playing > 41
-"`;
+const cond = `null`;
 
 const output = "classification";
 
